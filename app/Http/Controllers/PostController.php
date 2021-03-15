7a5fc2fc607fc2be9111ccc25b6af769d9ca9b5b
programarivm <info@programarivm.com>
2021-03-15 11:28:21 +0100

    Created PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function relevance(Request $request)
+    {
+        echo 'TODO';
+
+        exit;
+    }
+}
