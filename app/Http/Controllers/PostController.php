83a4e750d2668c06b317e3f9bcc4fc413d4e91b7
programarivm <info@programarivm.com>
2021-03-15 12:00:03 +0100

    Fetched posts from the JSONPlaceholder API

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class PostController extends Controller
 {
     public function relevance(Request $request)
     {
-        echo 'TODO';
+        $response = Http::get(env('FAKE_API_URL').'/posts');
+
+        $body = $response->body();
+
+        $arr = json_decode($body);
+
+        // TODO
+
+        print_r($arr);
 
         exit;
     }
